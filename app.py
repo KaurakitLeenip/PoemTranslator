a356ff467aef580bda0bb9390d8f8693f69724c8
Kaurakit <kaurakit@gmail.com>
2019-11-08 05:04:59 -0500

    added translate functionality and index will now update instead of a seperate page

--- app.py
+++ app.py
@@ -1,50 +1,50 @@
 from flask import Flask, render_template, request
 from google.cloud import translate_v2 as translater
+from google.oauth2 import service_account
 import random
-import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/kleenip/Downloads/Translate-8c4224ab5c22.json"
+
 app = Flask(__name__)
+credentials = service_account.Credentials.from_service_account_file(
+    '/home/kleenip/Downloads/translateapipoem-2c244528fce0.json'
+)
+translate_client = translater.Client(credentials=credentials)
 
 @app.route('/')
 def main():
-    # return render_template('index.html')
-    pass
+    return render_template('index.html', poem=[])
+    # pass
 
-# @app.route('/result', methods = ['POST'])
+@app.route('/result', methods=['GET', 'POST'])
 def get_result():
-    poem = request.form['raw_input']
-    iterations = request.form['num_iterations']
+    poem = request.form['raw_input'].split('\r\n')
+    res = []
+    iterations = int(request.form['num_iterations'])
+    # print(poem, iterations)
+    for line in poem:
+        res.append(translate(line, iterations))
+
+    return render_template('index.html', poem=res, original='\r\n'.join(poem))
+
 
 def translate(raw, iterations):
-    translate_client = translater.Client()
     res = raw
     for i in range(iterations):
-        res = (translate_client.translate(raw, get_language()))['translatedText']
-        res = translate(res, 'en')['translatedText']
+        target_lang = get_language()
+        print("translating to language: {1}".format(res, target_lang))
+        res = (translate_client.translate(raw, target_lang))['translatedText']
+    res = translate_client.translate(res, 'en')['translatedText']
+    return res
 
 def get_language():
-    languages = ['af','sq','am','ar','hy',
-                 'az','eu','be','bn','bs',
-                 'bg','ca','ceb','zh','zh-TW',
-                 'co','hr','cs','da','nl','er',
-                 'fi','fr','fy','gl','ka','de',
-                 'el','gu','ht','ha','haw','he',
-                 'hi','hmn','hu','is','ig','id',
-                 'ga','it','ja','jw','kn','kk',
-                 'km','ko','ku','ky','lo','la',
-                 'lv','lt','lb','mk','mg','ms',
-                 'ml','mt','mi','mr','mn','my',
-                 'mn','my','ne','no','ny','ps',
-                 'fa','pl','pt','pa','ro','ru',
-                 'sm','gd','sr','st','sn','sd',
-                 'sd','si','sk','sl','so','es',
-                 'su','sw','sv','tl','tg','ta',
-                 'te','th','tr','uk','ur','uz',
-                 'vi','cy','xh','yi','yo','zu']
-    return random.choice(languages)
+    results = translate_client.get_languages()
+    choice = random.choice(results)
+    return choice['language']
 
-
-translate("testing the program", 3)
+test = """Infinite in mystery is the gift of the Goddess
+We seek it thus, and take to the sky
+Ripples form on the water's surface
+The wandering soul knows no rest."""
+# print(translate(test, 50))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
